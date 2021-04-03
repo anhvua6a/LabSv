a035a81c6e2ac873d9e60ef31922a023814a7a60
anhvua6a <lhieu2000@gmail.com>
2021-04-03 17:11:54 +0700

    lab5

--- app.js
+++ app.js
@@ -1,48 +1,80 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var uploadRouter = require('./routes/upload')
+
+var app = express()
+
+
+var storage = multer.diskStorage({
+  destination: function(req,file,cb){
+    // đưa file vào thư mục chỉ định
+    cb(null,'public/images')
+    
+  },
+  filename:function(req,file,cb){
+    cb(null,file.originalname)
+  }
+})
+var upload = multer({storage,limits:{fileSize:1*1024*1024}})
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 app.get('/login',function(req,res){
   res.render('../views/login')
-});
+})
+let uploadSingle = upload.single("avatar")
+app.post('/upload',(req,res)=>{
+  uploadSingle(req, res, (err)=> {
+      if (err instanceof multer.MulterError) {
+        res.send('file không đc quá 1mb')
+        return
+      }
+      res.send('thành công')
+  })
+})
+
+app.post('/uploadMulti',upload.array('avatar', 10),(req,res,next)=>{
+  console.log(req.files)
+  res.send('up multi thành công')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+
+
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  process.argv.forEach(function(arg, index) {
+    console.log("argv[" + index + "] = " + arg);
+    });
   res.render('index', { title: 'Express' });
 });
 
