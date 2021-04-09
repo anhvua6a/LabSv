6cd6f973ae4c21f000e8fb3a4240f75a091dc505
anhvua6a <lhieu2000@gmail.com>
2021-04-09 21:47:26 +0700

    update

--- app.js
+++ app.js
@@ -22,9 +22,24 @@ var storage = multer.diskStorage({
   },
   filename:function(req,file,cb){
     cb(null,file.originalname)
-  }
+
+  },
 })
-var upload = multer({storage,limits:{fileSize:1*1024*1024}})
+var upload = multer({storage,limits:{fileSize:2*1024*1024}, fileFilter: (req, file, cb)=> {
+  if (file.mimetype != 'image/jpeg') {
+    cb(new Error('K đúng định dạng ảnh'));
+  } else {
+    cb(null, true)
+  }
+}})
+let uploads = multer({storage,limits:{files:5, fileSize:2*1024*1024}, fileFilter: (req, file, cb)=> {
+  
+ if (file.mimetype != 'image/jpeg') {
+    cb(new Error('K đúng định dạng ảnh'));
+  } else {
+    cb(null, true)
+  }
+}})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -40,23 +55,43 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
-app.get('/login',function(req,res){
-  res.render('../views/login')
+app.get('/upload',function(req,res){
+  res.render('upload')
 })
 let uploadSingle = upload.single("avatar")
 app.post('/upload',(req,res)=>{
   uploadSingle(req, res, (err)=> {
-      if (err instanceof multer.MulterError) {
-        res.send('file không đc quá 1mb')
+    if (err instanceof multer.MulterError) {
+        res.send('file không đc quá 2mb')
         return
-      }
-      res.send('thành công')
+    } else if (err) {
+      res.send(err.message)
+      return
+    }
+    console.log(req.file)
+    res.send('thành công')
   })
 })
 
-app.post('/uploadMulti',upload.array('avatar', 10),(req,res,next)=>{
+let uploadArr = uploads.array('avatar')
+app.post('/uploadMulti',(req,res,next)=>{
+  uploadArr(req, res, (err)=> {
+     if (err instanceof multer.MulterError) {
+       if (err.code == 'LIMIT_FILE_COUNT') {
+          res.send('chỉ có thể up tối đa 5 ảnh!')
+          return
+       } else if (err.code == 'LIMIT_FILE_SIZE') {
+        res.send('chỉ có thể up ảnh nhỏ hơn 2MB !')
+        return
+       }
+  } else if (err) {
+    res.send(err.message)
+    return
+  }
   console.log(req.files)
   res.send('up multi thành công')
+  })
+ 
 })
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  process.argv.forEach(function(arg, index) {
-    console.log("argv[" + index + "] = " + arg);
-    });
   res.render('index', { title: 'Express' });
 });
 
